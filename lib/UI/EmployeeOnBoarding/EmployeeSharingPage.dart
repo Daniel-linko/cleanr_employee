fa99274e855318b78f80bc0714f0f90606bccc84
walterwartenweiler <walter.wartenweiler@linkonomics.com>
2021-07-05 18:54:35 +0200

    Making sure the EmployeeInformationModel is loaded

--- lib/UI/EmployeeOnBoarding/EmployeeSharingPage.dart
+++ lib/UI/EmployeeOnBoarding/EmployeeSharingPage.dart
@@ -1,8 +1,10 @@
 import 'package:clean_r/Base/Sharing.dart';
 import 'package:clean_r/UI/Base/CleanRSkin.dart';
+import 'package:clean_r/UI/Base/NullWidget.dart';
 import 'package:clean_r/UI/Base/SharingPage.dart';
 import 'package:clean_r/localization/AppLocalization.dart';
 import 'package:cleanr_employee/Model/Employee.dart';
+import 'package:cleanr_employee/Model/EmployeeInformationModel.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -23,77 +25,86 @@ class EmployeeSharingPage extends StatelessWidget {
       String shareDescriptionTag,
       String domain,
       String linkType) {
-    String displayName = "Undefined";
-    if (employee.employeeInformationModel?.firstNameAttribute?.value != null)
-      displayName =
-          employee.employeeInformationModel?.firstNameAttribute?.value ??
-              "Undefined";
-    if (employee.employeeInformationModel?.lastNameAttribute?.value != null)
-      displayName = displayName +
-          " " +
-          (employee.employeeInformationModel?.lastNameAttribute?.value ?? "");
-
-    return FutureBuilder<Uri>(
-      future: Sharing.createDynamicLink(employee.userID(), androidPackage,
-          iosPackage, iosAppStoreID, linkType, domain, displayName),
-      builder: (context, shortLink) {
-        //if (shortLink.hasData) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
-          child: ElevatedButton(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 15, horizontal: 8),
-              child: Text(
-                AppLocalizations.of(context).translate(textTag),
-                textScaleFactor: CleanRSkin.designRatio(context),
-                textAlign: TextAlign.start,
-                style: TextStyle(
-                    fontFamily: CleanRSkin.buttonMainFont(),
-                    fontSize: 24,
-                    color: CleanRSkin.buttonTextColor(context)),
-              ),
-            ),
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) {
-                return SharingPage(
-                  shortLink: shortLink.data!,
-                  shareDefaultTextTag: shareDefaultTextTag,
-                  shareDescriptionTag: shareDescriptionTag,
-                  shareDefaultSubjectTag: shareDefaultSubjectTag,
+    return StreamBuilder<Iterable<EmployeeInformationModel>>(
+        stream: employee.employeeInformationModelStream(context),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            EmployeeInformationModel employeeInformationModel =
+                snapshot.data!.first;
+            String displayName = "Undefined";
+            if (employeeInformationModel.firstNameAttribute?.value != null)
+              displayName =
+                  employeeInformationModel.firstNameAttribute?.value ??
+                      "Undefined";
+            if (employeeInformationModel.lastNameAttribute?.value != null)
+              displayName = displayName +
+                  " " +
+                  (employeeInformationModel.lastNameAttribute?.value ?? "");
+            return FutureBuilder<Uri>(
+              future: Sharing.createDynamicLink(
+                  employee.userID(),
+                  androidPackage,
+                  iosPackage,
+                  iosAppStoreID,
+                  linkType,
+                  domain,
+                  displayName),
+              builder: (context, shortLink) {
+                //if (shortLink.hasData) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+                  child: ElevatedButton(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          vertical: 15, horizontal: 8),
+                      child: Text(
+                        AppLocalizations.of(context).translate(textTag),
+                        textScaleFactor: CleanRSkin.designRatio(context),
+                        textAlign: TextAlign.start,
+                        style: TextStyle(
+                            fontFamily: CleanRSkin.buttonMainFont(),
+                            fontSize: 24,
+                            color: CleanRSkin.buttonTextColor(context)),
+                      ),
+                    ),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) {
+                        return SharingPage(
+                          shortLink: shortLink.data!,
+                          shareDefaultTextTag: shareDefaultTextTag,
+                          shareDescriptionTag: shareDescriptionTag,
+                          shareDefaultSubjectTag: shareDefaultSubjectTag,
+                        );
+                      }));
+                    },
+                    style: ButtonStyle(
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(5.0),
+                      )),
+                      backgroundColor:
+                          CleanRSkin.buttonBackgroundColorMSP(context),
+                      overlayColor: MaterialStateProperty.resolveWith<Color>(
+                        (Set<MaterialState> states) {
+                          if (states.contains(MaterialState.disabled))
+                            return CleanRSkin.buttonBackgroundColorDisabled(
+                                context);
+                          else
+                            return CleanRSkin.buttonBackgroundColor(
+                                context); // Defer to the widget's default.
+                        },
+                      ),
+                    ),
+                  ),
                 );
-              }));
-            },
-            style: ButtonStyle(
-              shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0),
-              )),
-              backgroundColor: CleanRSkin.buttonBackgroundColorMSP(context),
-              overlayColor: MaterialStateProperty.resolveWith<Color>(
-                (Set<MaterialState> states) {
-                  if (states.contains(MaterialState.disabled))
-                    return CleanRSkin.buttonBackgroundColorDisabled(context);
-                  else
-                    return CleanRSkin.buttonBackgroundColor(
-                        context); // Defer to the widget's default.
-                },
-              ),
-            ),
-          ),
-        );
-        /*} else {
-          return Center(
-            child: Material(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Text(AppLocalizations.of(context)
-                    .translate("LoadingEmployeeSharing")),
-              ),
-            ),
-          );
-        }*/
-      },
-    );
+              },
+            );
+          } else {
+            return NullWidget();
+          }
+        });
   }
 
   @override
