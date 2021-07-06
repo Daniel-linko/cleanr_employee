0d55d8fe667524de2c7e1c68f9374d534565f837
walterwartenweiler <walter.wartenweiler@linkonomics.com>
2021-07-06 19:16:10 +0200

    Commit issue. Fixing.

--- lib/Model/Employee.dart
+++ lib/Model/Employee.dart
@@ -7,7 +7,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dash_chat/dash_chat.dart';
 import 'package:flutter/cupertino.dart';
 
-import 'EmployeeContactModel_.dart';
+import 'EmployeeContactModel.dart';
 
 class Employee extends ModelObject implements DataChangeObserver, CleanRUser {
   // field names
