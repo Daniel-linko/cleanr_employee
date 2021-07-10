70552fea969bb37f0f03795658636ad142a2ec16
walterwartenweiler <walter.wartenweiler@linkonomics.com>
2021-07-10 19:23:06 +0200

    Fixing missing , in lang resources

--- lib/Model/EmployeeContactModel.dart
+++ lib/Model/EmployeeContactModel.dart
@@ -2,10 +2,8 @@ import 'package:clean_r/Base/CleanRUser.dart';
 import 'package:clean_r/Base/DataChangeObserver.dart';
 import 'package:clean_r/Model/Base/BooleanAttribute.dart';
 import 'package:clean_r/Model/Base/ContactModel.dart';
-import 'package:clean_r/Model/Base/EmailAttribute.dart';
 import 'package:clean_r/Model/Base/EnumAttribute.dart';
 import 'package:clean_r/Model/Base/NumericAttribute.dart';
-import 'package:clean_r/Model/Base/TextAttribute.dart';
 import 'package:clean_r/Model/Base/ZipCodeAttribute.dart';
 import 'package:cleanr_employee/UI/Employee/PermitType.dart';
 
@@ -14,28 +12,22 @@ import 'Employee.dart';
 class EmployeeContactModel extends ContactModel {
   final Employee employee;
 
-  static const String _emailDBField = 'Email';
   static const String _zipCodeDBField = 'Zip Code';
   static const String _ironingSkillDBField = 'Ironing Skill';
   static const String _minWeeklyHoursDBField = 'Min Weekly Hours';
   static const String _maxWeeklyHoursDBField = 'Max Weekly Hours';
-  static const String _phoneDBField = 'Phone';
   static const String _nationalityPermitDBField = 'Nationality/Permit';
-  EmailAttribute? emailAttribute;
   ZipCodeAttribute? zipCodeAttribute;
   BooleanAttribute? ironingSkill;
   NumericAttribute? minWeeklyHours;
   NumericAttribute? maxWeeklyHours;
-  TextAttribute? phoneNumberAttribute;
   EnumAttribute<NationalityPermitType>? nationalityPermit;
 
   EmployeeContactModel(this.employee) : super() {
-    emailAttribute = EmailAttribute(this, null);
     zipCodeAttribute = ZipCodeAttribute(this, null);
     ironingSkill = BooleanAttribute(this, null);
     minWeeklyHours = NumericAttribute(this, null, 1);
     maxWeeklyHours = NumericAttribute(this, null, 1);
-    phoneNumberAttribute = TextAttribute(this, null);
     nationalityPermit = EnumAttribute<NationalityPermitType>(
         this, null, NationalityPermitType.permitTypes.values.first);
   }
@@ -47,18 +39,14 @@ class EmployeeContactModel extends ContactModel {
 
   @override
   void addToMap(Map<String, dynamic> map) {
-    map.addAll({_emailDBField: emailAttribute!.value});
     map.addAll({_ironingSkillDBField: ironingSkill!.value});
     map.addAll({_minWeeklyHoursDBField: minWeeklyHours!.value});
     map.addAll({_maxWeeklyHoursDBField: maxWeeklyHours!.value});
-    map.addAll({_phoneDBField: phoneNumberAttribute!.value});
     map.addAll({_nationalityPermitDBField: nationalityPermit!.value});
   }
 
   void fromMap(Map<String, dynamic> map, DataChangeObserver? observer) {
     super.fromMap(map, observer);
-    emailAttribute =
-        EmailAttribute.fromMap(map, _emailDBField, true, this, null);
     zipCodeAttribute =
         ZipCodeAttribute.fromMap(map, _zipCodeDBField, true, this, null);
     ironingSkill =
@@ -67,8 +55,6 @@ class EmployeeContactModel extends ContactModel {
         map, _minWeeklyHoursDBField, true, 1, this, null);
     maxWeeklyHours = NumericAttribute.fromMap(
         map, _maxWeeklyHoursDBField, true, 1, this, null);
-    phoneNumberAttribute =
-        TextAttribute.fromMap(map, _phoneDBField, true, this, null);
     nationalityPermit = EnumAttribute<NationalityPermitType>.fromMap(
         map,
         _nationalityPermitDBField,
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:clean_r/Expressions/Price%20Calculator/ServicePriceCalculator.dart';
 import 'package:clean_r/UI/Base/CleanRSkin.dart';
 import 'package:clean_r/localization/AppLocalization.dart';
-import 'package:cleanr_employee/UI/Employee/KarmaPage.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -20,7 +19,7 @@ String? oldEmployeeID;
 const String WelcomePageName = "WelcomePage";
 const String EmployeeInformationPageName = "EmployeeInformationPage";
 const String EmployeeSharingPageName = "EmployeeSharingPage";
-const String EmployeeKarmaPageName="EmployeeKarmaPage";
+const String EmployeeKarmaPageName = "EmployeeKarmaPage";
 String currentPageName = WelcomePageName;
 ValueNotifier<String> currentPage = new ValueNotifier<String>(WelcomePageName);
 
